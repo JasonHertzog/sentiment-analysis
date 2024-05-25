91343b77ada763b703f4b4b1e543e1620f0ef8ac
Jason Hertzog <jhertzog93@gmail.com>
2024-05-25 11:34:52 -0400

    Added preprocess.py and train_model.py

---
+++ train_model.py
@@ -0,0 +1,20 @@
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Directory to the data
+dir = "data/"
+
+# Load the preprocessed data
+X_train, X_test, y_train, y_test = joblib.load(dir+'data.pkl')
+
+# Train the model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Evaluate the model
+y_pred = model.predict(X_test)
+print('Accuracy:', accuracy_score(y_test, y_pred))
+
+# Save the model
+joblib.dump(model, dir+'sentiment_model.pkl')
