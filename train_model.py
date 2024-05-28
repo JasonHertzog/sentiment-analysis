4b3efa2d4ec5ece1604b97dea7735fb18676eede
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2024-05-28 16:22:54 -0400

    Cleaning up.

--- train_model.py
+++ train_model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,7 +12,7 @@ def train_model(file_path, model_path, vectorizer_path):
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
 
-    model = LogisticRegression(max_iter=500)
+    model = LogisticRegression(max_iter=1500)
     model.fit(X_train_vec, y_train)
     accuracy = model.score(X_test_vec, y_test)
     print(f"Model Accuracy: {accuracy}")
@@ -23,26 +22,3 @@ def train_model(file_path, model_path, vectorizer_path):
 
 if __name__ == "__main__":
     train_model('data/imdb_preprocessed.csv', 'sentiment_model_imdb.joblib', 'vectorizer_imdb.joblib')
-=======
-import joblib
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import config
-
-# Directory to the data
-dir = config.dir
-
-# Load the preprocessed data
-X_train, X_test, y_train, y_test = joblib.load(dir+'data.pkl')
-
-# Train the model
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-# Evaluate the model
-y_pred = model.predict(X_test)
-print('Accuracy:', accuracy_score(y_test, y_pred))
-
-# Save the model
-joblib.dump(model, dir+'sentiment_model.pkl')
->>>>>>> 1dba52abeb86358e14ad09c41672dfc8341ea0ac
