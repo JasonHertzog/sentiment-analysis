673889d00b859a7f3e64e2f1c01b71e9fc3f8c08
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2024-05-28 16:21:29 -0400

    Cleaning up.

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,4 @@
 import pandas as pd
-<<<<<<< HEAD
 
 def preprocess_imdb():
     data = pd.read_csv('data/IMDB Dataset.csv')
@@ -25,28 +24,3 @@ def preprocess_all():
 
 if __name__ == "__main__":
     preprocess_all()
-=======
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-import joblib
-import config
-
-# Get directory from config
-dir = config.dir
-
-# Load the dataset
-data = pd.read_csv(dir + 'IMDB Dataset.csv')
-
-# Preprocess the text data
-vectorizer = CountVectorizer(stop_words='english')
-X = vectorizer.fit_transform(data['review'])
-y = data['sentiment'].apply(lambda x: 1 if x == 'positive' else 0)
-
-# Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Save the preprocessed data
-joblib.dump((X_train, X_test, y_train, y_test),dir+'data.pkl')
-joblib.dump(vectorizer, dir+'vectorizer.pkl')
-
->>>>>>> 1dba52abeb86358e14ad09c41672dfc8341ea0ac
