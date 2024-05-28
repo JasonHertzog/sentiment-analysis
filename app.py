1896144b9ec875bb84f5e120e074ab0305483525
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2024-05-28 16:17:24 -0400

    Cleaning up.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, render_template
 import joblib
 
@@ -31,29 +30,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-=======
-from flask import Flask, render_template, request, jsonify
-import joblib
-import config
-
-app = Flask(__name__)
-dir = config.dir
-model = joblib.load(dir+'sentiment_model.pkl')
-vectorizer = joblib.load(dir+'vectorizer.pkl')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    review = data['review']
-    transformed_review = vectorizer.transform([review])
-    prediction = model.predict(transformed_review)[0]
-    return jsonify({'sentiment': 'positive' if prediction == 1 else 'negative'})
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-
->>>>>>> 1dba52abeb86358e14ad09c41672dfc8341ea0ac
